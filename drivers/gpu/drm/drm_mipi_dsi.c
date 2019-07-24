3a2c291ce265f11c518f34e8af307ce6285abc5c
Bang Nguyen <bangnguyen@motorola.com>
2019-07-24 07:55:25 -0500

    drm/dsi: Implement DCS set disp brightness 2bytes
    Provide a small convenience wrapper that set the display 2bytes
    brightness
    
    Port from: 4a5c926
    
    Change-Id: Iadfcb1d6cdf4866391f36ee970d79840e109fe9a
    Signed-off-by: Bang Nguyen <bangnguyen@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1410287
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Submit-Approved: Jira Key
    Reviewed-on: https://gerrit.mot.com/1824045
    Reviewed-by: Ling Jin <lingjin@motorola.com>

--- drivers/gpu/drm/drm_mipi_dsi.c
+++ drivers/gpu/drm/drm_mipi_dsi.c
@@ -1116,6 +1116,29 @@ int mipi_dsi_dcs_set_display_brightness_large(struct mipi_dsi_device *dsi,
 EXPORT_SYMBOL(mipi_dsi_dcs_set_display_brightness_large);
 
 /**
+ * mipi_dsi_dcs_set_display_brightness_2bytes() - sets the brightness value of
+ *    the display with 2bytes value
+ * @dsi: DSI peripheral device
+ * @brightness: brightness value
+ *
+ * Return: 0 on success or a negative error code on failure.
+ */
+int mipi_dsi_dcs_set_display_brightness_2bytes(struct mipi_dsi_device *dsi,
+					u16 brightness)
+{
+	u8 payload[2] = { (brightness & 0xff00) >> 8, brightness & 0xff};
+	ssize_t err;
+
+	err = mipi_dsi_dcs_write(dsi, MIPI_DCS_SET_DISPLAY_BRIGHTNESS,
+				payload, sizeof(payload));
+	if (err < 0)
+		return err;
+
+	return 0;
+}
+EXPORT_SYMBOL(mipi_dsi_dcs_set_display_brightness_2bytes);
+
+/**
  * mipi_dsi_dcs_get_display_brightness_large() - gets the current 16-bit
  *    brightness value of the display
  * @dsi: DSI peripheral device
@@ -1143,6 +1166,9 @@ int mipi_dsi_dcs_get_display_brightness_large(struct mipi_dsi_device *dsi,
 	return 0;
 }
 EXPORT_SYMBOL(mipi_dsi_dcs_get_display_brightness_large);
+=======
+EXPORT_SYMBOL(mipi_dsi_dcs_set_display_brightness_2bytes);
+>>>>>>> 20394ee9a6eb3 (drm/dsi: Implement DCS set disp brightness 2bytes)
 
 static int mipi_dsi_drv_probe(struct device *dev)
 {
